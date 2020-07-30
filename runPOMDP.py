dffd23924b7acc22ada4c12ca6319cd128ac0070
Ziyue Wu <37011471+Tinky2013@users.noreply.github.com>
2020-07-30 19:52:46 +0800

    Add files via upload

--- runPOMDP.py
+++ runPOMDP.py
@@ -1,10 +1,52 @@
+from src.tigermodel import TigerModel
+from src.pbvi import PBVI
+
 def main():
     execParams = {
         'config': 'pbvi',
-        'random_prior': False,
-        'max_play': 100,
         'model_name': 'Tiger',
+        'max_play': 100,
     }
+
+    algoParams = {
+        'algo':'pbvi',
+        'T': 5,
+    }
+
+    modelName = execParams['model_name']
+    maxPlay = execParams['max_play']
+    algoName = algoParams['algo']
+    horizonT = algoParams['T']
+    modelEnv = None     # model environment
+    solve = None        # solver
+
+    print("Initialising-------------")
+
+    # here we choose the task environment and the solver
+    if modelName == 'Tiger':
+        modelEnv = TigerModel()
+    if algoName == 'pbvi':
+        solve = PBVI()
+
+
+    # here we generate the initial belief
+    belief = modelEnv.generateUniformBeliefs()
+    totalRewards = 0
+
+    print('''
+    *************************
+    Initial State:   {}
+    Initial Belief:  {}
+    Time Horizon:    {}
+    Max Play:        {}
+    *************************
+    '''.format(modelEnv.currentState,belief,horizonT,maxPlay))
+
+    # start playing
+    for i in range(maxPlay):
+        # TODO: write the main part of the solving
+        pass
+
 
 if __name__ == '__main__':
     main()
