ab734253949a9c53d51f358a585a95cedd03af5a
Ziyue Wu <37011471+Tinky2013@users.noreply.github.com>
2020-08-02 22:06:56 +0800

    Add files via upload

---
+++ tools/sampleUtility.py
@@ -0,0 +1,26 @@
+import numpy as np
+import random
+from array import array
+from numpy import *
+
+np.random.seed()
+random.seed()
+
+def chooseItemIdx(probs):
+    assert(abs(sum(probs) - 1.0) <= 0.0000000)
+    probs = np.array(probs)
+    return np.random.choice(list(range(len(probs))), p=probs/probs.sum())
+
+def generateBeliefPoints(states, stepsize):
+    '''
+    Here we used Uniform Sampling
+    '''
+    beliefPoints = [
+        [random.uniform() for s in states] for i in arange(0, 1+stepsize, stepsize)
+    ]
+    return array(beliefPoints)
+
+def generateUniformBeliefs(states):
+    stateNUM = len(states)
+    Beliefs = [1/stateNUM for i in range(stateNUM)]
+    return Beliefs
