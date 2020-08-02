4f336a4cf8705b4d0aadb2ff2a8f448ac72199f0
Ziyue Wu <37011471+Tinky2013@users.noreply.github.com>
2020-08-02 22:07:15 +0800

    Add files via upload

--- runPOMDP.py
+++ runPOMDP.py
@@ -1,6 +1,8 @@
 from src.tigermodel import TigerModel
 from src.pbvi import PBVI
 
+from tools.sampleUtility import generateBeliefPoints, generateUniformBeliefs
+
 def main():
     execParams = {
         'config': 'pbvi',
@@ -12,6 +14,7 @@ def main():
     algoParams = {
         'algo':'pbvi',
         'T': 5,
+        'step_size': 0.01,
     }
 
     modelName = execParams['model_name']
@@ -19,37 +22,44 @@ def main():
     algoName = algoParams['algo']
     horizonT = algoParams['T']
     modelEnv = None     # model environment
-    solve = None        # solver
+    solver = None        # solver
 
-    print("Initialising-------------")
+    print("Initialising...")
 
     # here we choose the task environment and the solver
     if modelName == 'Tiger':
         modelEnv = TigerModel()
     if algoName == 'pbvi':
-        solve = PBVI(modelEnv)
+        solver = PBVI(modelEnv)
 
-    # here we generate the initial belief
-    belief = modelEnv.generateUniformBeliefs()
+    # here we generate the initial belief and choose some belief points
+    belief = generateUniformBeliefs(modelEnv.states)
+    beliefPoints = generateBeliefPoints(modelEnv.states, algoParams['step_size'])
+    solver.specifyAlgorithmArguments(beliefPoints)
+
     totalRewards = 0
     if execParams['print_process']:
         print('''Initial State: {} || Initial Belief: {} || Time Horizon: {} || Max Play: {}
         '''.format(modelEnv.currentState,belief,horizonT,maxPlay))
 
+
+
     # start playing
     for i in range(maxPlay):
         # this is a general framework of solving POMDP problems
-        solve.solveHorizonT(horizonT)                               # planning
-        action = solve.getBestAction(belief)                        # get best action
-        nextState, observation, reward = None, None, None           # receive environment feedback
-        belief = solve.updateBelief(belief, action, observation)    # update the belief
-        totalRewards += 1
+        solver.solveHorizonT(horizonT)                               # planning
+        action = solver.getBestAction(belief)                        # get best action
+        nextState, observation, reward = solver.envFeedback(action)  # receive environment feedback
+        belief = solver.updateBelief(belief, action, observation)    # update the belief
+        totalRewards += reward
 
         # for every trial, print the result
         if execParams['print_process']:
             print("Play Times: {} || Action Chosen: {} || Observation: {} || Reward: {} || New State: {} || New Belief: {}".format(i,action,observation,reward,nextState,belief))
 
     print("Total reward:{}".format(totalRewards))
+
+
 
 if __name__ == '__main__':
     main()
