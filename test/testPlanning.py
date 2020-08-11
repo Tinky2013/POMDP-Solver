c8bdae1840cbe7d14adf67c9fdfd7e04160261e7
Tinky2013 <670122025@qq.com>
2020-08-11 13:00:03 +0800

    test planning

--- test/testPlanning.py
+++ test/testPlanning.py
@@ -24,14 +24,18 @@ class TestPlanning(unittest.TestCase):
 
     @data(
         ([0.5,0.5],'listen'),
+        ([0.500001,0.499999],'listen'),
+        ([0.499999,0.500001],'listen'),
+        ([0.6, 0.4], 'listen'),
+        ([0.4, 0.6], 'listen'),
+
         ([1,0],'open-right'),
         ([0,1],'open-left'),
-        ([0.6,0.4],'listen'),
-        ([0.4,0.6],'listen'),
-        ([0.995,0.005],'open-right'),
-        ([0.005,0.995],'open-left'),
         ([0.0000001,0.9999999],'open-left'),
         ([0.9999999,0.0000001],'open-right'),
+
+        ([0.995, 0.005], 'open-right'),
+        ([0.005, 0.995], 'open-left'),
     )
     @unpack
     def testGetBestPlanningAction(self, belief, actualBestAction):
