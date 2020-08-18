886b3963a728b53fefbe0a09a54356bda7e6788b
Ziyue Wu <37011471+Tinky2013@users.noreply.github.com>
2020-08-18 10:48:12 +0800

    Update runPomdpExample.py

--- runPomdpExample.py
+++ runPomdpExample.py
@@ -1,12 +1,9 @@
 from src.tigermodel import TigerModel
 from src.pbvi import PBVI
 from src.pomdputility import EnvFeedback, UpdateBelief
-
 from tools.sampleUtility import generateInitBeliefPoints, generateUniformBeliefs
-
 from visualization.visualizeTiger import VisualizedTiger
 import datetime
-
 
 def main():
     execParams = {
@@ -35,44 +32,43 @@ def main():
         }
     }
 
-
+    # initialize environment
     modelEnv = TigerModel()
     modelEnv.specifyEnvironmentArguments(envParams)
-    visualizer= VisualizedTiger()
-    solver = PBVI(modelEnv)
     envFeedback = EnvFeedback(modelEnv)
     updateBelief = UpdateBelief(modelEnv)
-
+    
+    # initialize solver
+    solver = PBVI(modelEnv)
     beliefPoints = generateInitBeliefPoints(modelEnv.states, algoParams['num_belief'])
     solver.specifyAlgorithmArguments(beliefPoints,algoParams)
-
-    # 5. choose your visualization part
+    
+    # initialize visualization tools
+    #visualizer= VisualizedTiger()
     #visualizer.visualizeBeliefPoint(beliefPoints)
 
-
-    # start playing
     totalRewards = 0
     belief = generateUniformBeliefs(modelEnv.states)    # for model evaluation
     if execParams['print_process']:
         print('''Initial State: {} || Initial Belief: {} || Time Horizon: {} || Max Play: {}
             '''.format(modelEnv.currentState,belief,algoParams['horizon_T'],execParams['max_play']))
+    
+    # start playing
     starttime = datetime.datetime.now()
-
     for i in range(execParams['max_play']):
         # this is a general framework of solving POMDP problems
         action = solver.getPlanningAction(belief)       # get best action
         nextState, observation, reward = envFeedback(action)  # receive environment feedback
         belief = updateBelief(belief, action, observation)    # update the belief
         totalRewards += reward
-
         # for every trial, print the result
         if execParams['print_process']:
             print("Play Times: {} || Action Chosen: {} || Observation: {} || Reward: {} || New State: {} || New Belief: {}".format(i,action,observation,reward,nextState,belief))
-    print("Total reward:{}".format(totalRewards))
-    # record the playing time
+    # end playing
     endtime = datetime.datetime.now()
-    timeFloat = round((endtime - starttime).seconds+0.000001*(endtime - starttime).microseconds,2)
-    print("Playing time:  {} sec".format(timeFloat))
+    
+    print("Total reward:{}".format(totalRewards))
+    print("Playing time:  {} sec".format(round((endtime - starttime).seconds+0.000001*(endtime - starttime).microseconds,2)))
 
 
 if __name__ == '__main__':
